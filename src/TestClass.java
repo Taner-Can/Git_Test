76af0e4013f2c860ccf0e2f149c408fc5a0c7abb
Taner-Can <88287142+Taner-Can@users.noreply.github.com>
2021-09-12 12:05:34 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Welcome to Git");
          System.out.println("Welcome to GitHub");
+                 System.out.println("Welcome to GitHub for the second time");
+
     }
 }
